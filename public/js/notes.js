70abcf33eb682101df9686116bc6f53dd6be13a9
Audrey Lee <aul004@ucsd.edu>
2017-11-06 01:55:40 -0800

    done with notes widget

--- public/js/notes.js
+++ public/js/notes.js
@@ -5,10 +5,7 @@ function initNotes() {
 }
 
 function addNote(){
-    var notes = retrieveNotes();
     var timestamp = Math.round((new Date()).getTime() / 1000);
-    notes[timestamp] = "";
-
     saveNote(timestamp, "");
     populateNotesSummary();
     populateNoteContent(timestamp);
@@ -16,55 +13,61 @@ function addNote(){
 
 function saveNote(note, content) {
     var notes = retrieveNotes();
-
-    notes[note] = content;
+    if (!(note in notes)){
+        notes[note] = {};
+        notes[note]["identifier"] = "New Note";
+    }
+    notes[note]["content"] = content;
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
 function saveCurrentNote() {
-    var note = document.getElementById("notes_summary").value;
+    var note = document.getElementById("notes_identifier").value;
     var content = document.getElementById("notes_content").value;
     saveNote(note, content);
 }
 
+function changeFontSize(size) {
+    // $("#notes_identifier").css("font-size", size); // todo: do we want to change font size for the id as well?
+    $("#notes_content").css("font-size", size);
+}
 
 function retrieveNotes() {
     var notes = localStorage.getItem("notes");
-    if (notes != undefined) {
-        return JSON.parse(notes);
+    // check for undefined notes
+    if (notes == undefined) {
+        initNotes();
     }
-    return undefined;
+    return JSON.parse(notes);
 }
 
 // todo
 function populateNotesSummary() {
     var notes = retrieveNotes();
-    document.getElementById("notes_summary").innerHTML = "";
-    // todo: error check for undefined notes
+    document.getElementById("notes_identifier").innerHTML = "";
+
     for(const note in notes) {
         var note_summary = document.createElement("option");
-        note_summary.style="border-width:1"
-        note_summary.innerHTML = note;
-        document.getElementById("notes_summary").appendChild(note_summary);
+        note_summary.style="border-width:1";
+        note_summary.value = note;
+        note_summary.innerHTML = notes[note]["identifier"];
+        document.getElementById("notes_identifier").appendChild(note_summary);
     }
-    document.getElementById("notes_summary").onclick = function(){
-        console.log('changed to ' + this.value);
+
+    document.getElementById("notes_identifier").onclick = function(){
         populateNoteContent(this.value);
     }
 }
 
 function populateNoteContent(note) {
     var notes = retrieveNotes();
-    // todo: error check for undefined notes
-    document.getElementById("notes_content").value = notes[note];
+    document.getElementById("notes_content").value = notes[note]["content"];
 }
 
 function deleteNote() {
     var notes = retrieveNotes();
-    // todo: error check for undefined notes
-    var note = document.getElementById("notes_summary").value;
+    var note = document.getElementById("notes_identifier").value;
     if ((note != undefined) && (note != "") && (note in notes)) {
-        // todo: add a warning
         if(confirm("Are you sure you want to delete the current note?")) {
             delete notes[note];
             localStorage.setItem("notes", JSON.stringify(notes));
@@ -78,4 +81,7 @@ function deleteNote() {
 $(document).ready(function(){
     initNotes();
     populateNotesSummary();
+
+    // todo: think of better way to handle this
+    document.getElementById("notes_content").onchange = saveCurrentNote;
 });
