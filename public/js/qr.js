83cefb7245239589275cc71ab23b795e4c3df069
Audrey Lee <aul004@ucsd.edu>
2017-11-15 00:06:57 -0800

    nav bar reflects user. qr reflects user

--- public/js/qr.js
+++ public/js/qr.js
@@ -1,3 +1,17 @@
+function getCurrentUser() {
+    var users = localStorage.getItem("users");
+    var curruser = localStorage.getItem("user");
+    // check for undefined users
+    if (users != undefined) 
+        users = JSON.parse(users);
+        return users[curruser];
+}
+function getUsers() {
+    var users = localStorage.getItem("users");
+    // check for undefined notes
+    if (users != undefined) 
+        return JSON.parse(users);
+}
 $(document).ready(function(){
     var qrdialog = $( "#qr-dialog" ).dialog({
         dialogClass: "qrPopup",
@@ -17,6 +31,10 @@ $(document).ready(function(){
     });
 
     $(".qr").click(function() {
+        var curruser = getCurrentUser();
+        document.getElementById('qr-username').value = curruser['name'];
+        document.getElementById('qr-phone').value = curruser['phone'];
+        document.getElementById('qr-loc').value = curruser['loc'];
         qrdialog.dialog( "open" );
     });
 })
