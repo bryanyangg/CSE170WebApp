b3a8ca895a5f3f1951c295fe1ddd6eb04cf6aa5c
Audrey Lee <aul004@ucsd.edu>
2017-11-16 11:41:04 -0800

    begin working on splash page

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,9 +2,10 @@ function getCurrentUser() {
     var users = localStorage.getItem("users");
     var curruser = localStorage.getItem("user");
     // check for undefined users
-    if (users != undefined) 
+    if (users != undefined && curruser != undefined) {
         users = JSON.parse(users);
         return users[curruser];
+    }
 }
 function getUsers() {
     var users = localStorage.getItem("users");
@@ -17,10 +18,15 @@ $(document).ready(function(){
     console.log('document ready in nav');
     $('#nav').load('nav.html', function(){
         console.log("loaded the nav");
-        document.getElementById("nav-username").innerHTML = getCurrentUser()["name"];
-        document.getElementById("nav-logout").click = function(){
-            delete localStorage.user;
-            window.location.href = "login.html";
+        var curruser = getCurrentUser();
+        if (curruser != undefined) { // logged in
+            document.getElementById("nav-username").innerHTML = getCurrentUser()["name"];
+            document.getElementById("nav-logout").click = function(){
+                delete localStorage.user;
+                window.location.href = "login.html";
+            }
+        } else { // not logged in
+            document.getElementById("nav-username").innerHTML = "Login Sign Up"
         }
         
         // switch(window.location.href.split("/")[-1]) {
